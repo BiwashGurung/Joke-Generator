96db01340d8fa785d1f5deb6944391a4d88fc5be
BiwashGurung <varungrg10@gmail.com>
2023-08-22 17:29:16 +0545

    delcared a function getjoke

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const JokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,explicit&type=single";
 
+let getJoke = () => {
+    JokeContainer.classList.remove("fade");
+    
+}
