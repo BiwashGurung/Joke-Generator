a3bee6976f51500fea206b21568e7549d6700ba3
BiwashGurung <varungrg10@gmail.com>
2023-08-22 17:44:51 +0545

    added event listener for the specified event

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// script.js
 const JokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,explicit&type=single";
@@ -5,9 +6,25 @@ const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,polit
 let getJoke = () => {
     JokeContainer.classList.remove("fade");
     fetch(url)
-    .then(data => data.json())
-    .then(item => {
-        JokeContainer.textContent = `${item.joke}`;
-        JokeContainer.classList.toggle("fade");
-    })
-}
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(item => {
+            if (item.joke) {
+                JokeContainer.textContent = item.joke;
+            } else {
+                JokeContainer.textContent = "Oops! No joke found.";
+            }
+            JokeContainer.classList.add("fade");
+        })
+        .catch(error => {
+            console.error("Error fetching joke:", error);
+            JokeContainer.textContent = "Oops! An error occurred.";
+        });
+};
+
+btn.addEventListener("click", getJoke);
+
