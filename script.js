32ef17d5f80deb8393cc1099cf6e791064445646
BiwashGurung <varungrg10@gmail.com>
2023-08-22 17:28:29 +0545

    Feat :(API) added API url link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const JokeContainer = document.getElementById("joke");
 const btn = document.getElementById("btn");
-
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,explicit&type=single";
 
